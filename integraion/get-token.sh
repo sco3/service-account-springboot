58f542c2e37151ff2e2bbfdbabc2d5d77340e943
Dmitry Zakharov <dzakarov@gmail.com>
2026-06-29 11:06:46 +0100

    wf
    Signed-off-by: Dmitry Zakharov <dzakarov@gmail.com>

---
+++ integraion/get-token.sh
@@ -0,0 +1,7 @@
+curl -X POST "http://localhost:8081/realms/service-accounts/protocol/openid-connect/token" \
+  -H "Content-Type: application/x-www-form-urlencoded" \
+  -d "client_id=service-api" \
+  -d "client_secret=service-api-secret" \
+  -d "username=admin" \
+  -d "password=admin" \
+  -d "grant_type=password"
