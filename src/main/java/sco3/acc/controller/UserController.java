d143a97406e15536a601dae9d300551f1e455614
Dmitry Zakharov <dzakarov@gmail.com>
2026-06-30 06:27:17 +0100

    refactor
    Signed-off-by: Dmitry Zakharov <dzakarov@gmail.com>

--- src/main/java/sco3/acc/controller/UserController.java
+++ src/main/java/sco3/acc/controller/UserController.java
@@ -5,7 +5,6 @@ import static sco3.acc.common.AccConstants.API_V1;
 import java.util.Arrays;
 import java.util.List;
 
-
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import sco3.acc.dto.UserDto;
+import sco3.acc.model.User;
 import sco3.acc.service.UserService;
 
 @RestController
@@ -25,6 +25,14 @@ public class UserController {
 
 	public UserController(UserService service) {
 		this.service = service;
+	}
+
+	UserDto toUserDto(User user) {
+		return new UserDto( //
+				user.userId(), //
+				user.serviceAccount(), //
+				user.createdAt() //
+		);
 	}
 
 	@GetMapping("/users")
@@ -46,12 +54,7 @@ public class UserController {
 
 		if (serviceAccount != null && serviceAccount.size() > 0) {
 			return service.findByServiceAccounts(serviceAccount) //
-					.stream() //
-					.map(user -> new UserDto( //
-							user.userId(), //
-							user.serviceAccount(), //
-							user.createdAt() //
-					)).toList();
+					.stream().map(this::toUserDto).toList();
 
 		}
 
